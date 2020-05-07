cd12fa073856e3707b43f96604d4007b0267de7d
Asgeir Nilsen <asgeir@fintlabs.no>
2020-05-07 14:10:42 +0200

    Updated LinkMapper.linkMapper

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -5,11 +5,10 @@ import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.utdanning.elev.Elevforhold;
 import no.fint.model.utdanning.elev.Medlemskap;
 import no.fint.model.utdanning.elev.Undervisningsforhold;
+import no.fint.model.utdanning.kodeverk.Skolear;
+import no.fint.model.utdanning.kodeverk.Termin;
 import no.fint.model.utdanning.kodeverk.Vigoreferanse;
-import no.fint.model.utdanning.timeplan.Fag;
-import no.fint.model.utdanning.timeplan.Rom;
-import no.fint.model.utdanning.timeplan.Time;
-import no.fint.model.utdanning.timeplan.Undervisningsgruppe;
+import no.fint.model.utdanning.timeplan.*;
 import no.fint.model.utdanning.utdanningsprogram.Programomrade;
 import no.fint.model.utdanning.utdanningsprogram.Skole;
 import no.fint.model.utdanning.vurdering.Eksamensgruppe;
@@ -24,6 +23,9 @@ public class LinkMapper {
                 .put(Rom.class.getName(), contextPath + RestEndpoints.ROM)
                 .put(Time.class.getName(), contextPath + RestEndpoints.TIME)
                 .put(Undervisningsgruppe.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPE)
+                .put(Undervisningsgruppemedlemskap.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPEMEDLEMSKAP)
+                .put(Skolear.class.getName(), "/utdanning/kodeverk/skolear")
+                .put(Termin.class.getName(), "/utdanning/kodeverk/termin")
                 .put(Programomrade.class.getName(), "/utdanning/utdanningsprogram/programomrade")
                 .put(Skole.class.getName(), "/utdanning/utdanningsprogram/skole")
                 .put(Eksamensgruppe.class.getName(), "/utdanning/vurdering/eksamensgruppe")
