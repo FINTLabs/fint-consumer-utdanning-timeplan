b7aead41402a17344970ae8d8c6e5f8d3d06aca5
Asgeir Nilsen <asgeir@fintlabs.no>
2020-07-06 12:15:31 +0200

    Update interceptor matching rules.

--- src/main/java/no/fint/consumer/config/WebConfig.java
+++ src/main/java/no/fint/consumer/config/WebConfig.java
@@ -1,9 +1,10 @@
 package no.fint.consumer.config;
 
 import no.fint.consumer.utils.RequestHeaderInterceptor;
-import no.fint.consumer.utils.RestEndpoints;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -19,6 +21,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private Map<String, String> links;
 
+    @Value("${server.context-path:}")
+    private String contextPath;
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
@@ -29,7 +33,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestHeaderInterceptor(links.values()))
-                .addPathPatterns(links.values().stream().map(s -> s + "/**").peek(System.out::println).toArray(String[]::new));
+        registry.addInterceptor(new RequestHeaderInterceptor(links.values().stream().filter(s -> s.startsWith(contextPath)).collect(Collectors.toList())))
+                .addPathPatterns(links.values().stream().filter(s -> s.startsWith(contextPath)).map(s -> StringUtils.removeStart(s, contextPath)).toArray(String[]::new));
     }
 }
