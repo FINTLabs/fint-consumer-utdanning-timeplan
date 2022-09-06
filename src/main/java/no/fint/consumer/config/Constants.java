7d55dc60b0927869c382e3d681a5d6075154aaec
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2022-09-06 15:56:23 +0200

    Version 3.11.0-rc-3.

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -11,16 +11,19 @@ public enum Constants {
     public static final String CACHE_INITIALDELAY_FAG = "${fint.consumer.cache.initialDelay.fag:900000}";
     public static final String CACHE_FIXEDRATE_FAG = "${fint.consumer.cache.fixedRate.fag:900000}";
     
-    public static final String CACHE_INITIALDELAY_ROM = "${fint.consumer.cache.initialDelay.rom:1000000}";
+    public static final String CACHE_INITIALDELAY_FAGGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.faggruppemedlemskap:1000000}";
+    public static final String CACHE_FIXEDRATE_FAGGRUPPEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.faggruppemedlemskap:900000}";
+    
+    public static final String CACHE_INITIALDELAY_ROM = "${fint.consumer.cache.initialDelay.rom:1100000}";
     public static final String CACHE_FIXEDRATE_ROM = "${fint.consumer.cache.fixedRate.rom:900000}";
     
-    public static final String CACHE_INITIALDELAY_TIME = "${fint.consumer.cache.initialDelay.time:1100000}";
+    public static final String CACHE_INITIALDELAY_TIME = "${fint.consumer.cache.initialDelay.time:1200000}";
     public static final String CACHE_FIXEDRATE_TIME = "${fint.consumer.cache.fixedRate.time:900000}";
     
-    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.initialDelay.undervisningsgruppe:1200000}";
+    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.initialDelay.undervisningsgruppe:1300000}";
     public static final String CACHE_FIXEDRATE_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.fixedRate.undervisningsgruppe:900000}";
     
-    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.undervisningsgruppemedlemskap:1300000}";
+    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.undervisningsgruppemedlemskap:1400000}";
     public static final String CACHE_FIXEDRATE_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.undervisningsgruppemedlemskap:900000}";
     
 
