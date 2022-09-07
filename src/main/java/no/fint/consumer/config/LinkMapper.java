17863ec8d71fdf57063eb5ae838e387f7a6f9ca0
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2022-09-07 14:02:43 +0200

    Version 3.11.0. (#15)

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -4,6 +4,7 @@ import no.fint.consumer.utils.RestEndpoints;
 import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 import no.fint.model.utdanning.timeplan.Fag;
+import no.fint.model.utdanning.timeplan.Faggruppemedlemskap;
 import no.fint.model.utdanning.timeplan.Rom;
 import no.fint.model.utdanning.timeplan.Time;
 import no.fint.model.utdanning.timeplan.Undervisningsgruppe;
@@ -14,17 +15,19 @@ public class LinkMapper {
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String,String>builder()
             .put(Fag.class.getName(), contextPath + RestEndpoints.FAG)
+            .put(Faggruppemedlemskap.class.getName(), contextPath + RestEndpoints.FAGGRUPPEMEDLEMSKAP)
             .put(Rom.class.getName(), contextPath + RestEndpoints.ROM)
             .put(Time.class.getName(), contextPath + RestEndpoints.TIME)
             .put(Undervisningsgruppe.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPE)
             .put(Undervisningsgruppemedlemskap.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPEMEDLEMSKAP)
             .put("no.fint.model.utdanning.utdanningsprogram.Programomrade", "/utdanning/utdanningsprogram/programomrade")
             .put("no.fint.model.utdanning.elev.Elevtilrettelegging", "/utdanning/elev/elevtilrettelegging")
+            .put("no.fint.model.utdanning.timeplan.Faggruppe", "/utdanning/timeplan/faggruppe")
             .put("no.fint.model.utdanning.utdanningsprogram.Skole", "/utdanning/utdanningsprogram/skole")
             .put("no.fint.model.utdanning.vurdering.Eksamensgruppe", "/utdanning/vurdering/eksamensgruppe")
             .put("no.fint.model.utdanning.elev.Medlemskap", "/utdanning/elev/medlemskap")
-            .put("no.fint.model.utdanning.elev.Undervisningsforhold", "/utdanning/elev/undervisningsforhold")
             .put("no.fint.model.utdanning.elev.Elevforhold", "/utdanning/elev/elevforhold")
+            .put("no.fint.model.utdanning.elev.Undervisningsforhold", "/utdanning/elev/undervisningsforhold")
             .put("no.fint.model.utdanning.kodeverk.Termin", "/utdanning/kodeverk/termin")
             .put("no.fint.model.utdanning.kodeverk.Skolear", "/utdanning/kodeverk/skolear")
             /* .put(TODO,TODO) */
