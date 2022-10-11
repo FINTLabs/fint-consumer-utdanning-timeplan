552666c954e8ed83d7cdf2f1efa948284825b7f2
Nils-Odd Solberg <33060326+nilsodd@users.noreply.github.com>
2022-10-11 11:54:49 +0200

    Version 3.11.10. (#16)

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -11,19 +11,22 @@ public enum Constants {
     public static final String CACHE_INITIALDELAY_FAG = "${fint.consumer.cache.initialDelay.fag:900000}";
     public static final String CACHE_FIXEDRATE_FAG = "${fint.consumer.cache.fixedRate.fag:900000}";
     
-    public static final String CACHE_INITIALDELAY_FAGGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.faggruppemedlemskap:1000000}";
+    public static final String CACHE_INITIALDELAY_FAGGRUPPE = "${fint.consumer.cache.initialDelay.faggruppe:1000000}";
+    public static final String CACHE_FIXEDRATE_FAGGRUPPE = "${fint.consumer.cache.fixedRate.faggruppe:900000}";
+    
+    public static final String CACHE_INITIALDELAY_FAGGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.faggruppemedlemskap:1100000}";
     public static final String CACHE_FIXEDRATE_FAGGRUPPEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.faggruppemedlemskap:900000}";
     
-    public static final String CACHE_INITIALDELAY_ROM = "${fint.consumer.cache.initialDelay.rom:1100000}";
+    public static final String CACHE_INITIALDELAY_ROM = "${fint.consumer.cache.initialDelay.rom:1200000}";
     public static final String CACHE_FIXEDRATE_ROM = "${fint.consumer.cache.fixedRate.rom:900000}";
     
-    public static final String CACHE_INITIALDELAY_TIME = "${fint.consumer.cache.initialDelay.time:1200000}";
+    public static final String CACHE_INITIALDELAY_TIME = "${fint.consumer.cache.initialDelay.time:1300000}";
     public static final String CACHE_FIXEDRATE_TIME = "${fint.consumer.cache.fixedRate.time:900000}";
     
-    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.initialDelay.undervisningsgruppe:1300000}";
+    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.initialDelay.undervisningsgruppe:1400000}";
     public static final String CACHE_FIXEDRATE_UNDERVISNINGSGRUPPE = "${fint.consumer.cache.fixedRate.undervisningsgruppe:900000}";
     
-    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.undervisningsgruppemedlemskap:1400000}";
+    public static final String CACHE_INITIALDELAY_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.undervisningsgruppemedlemskap:1500000}";
     public static final String CACHE_FIXEDRATE_UNDERVISNINGSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.undervisningsgruppemedlemskap:900000}";
     
 
--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -4,6 +4,7 @@ import no.fint.consumer.utils.RestEndpoints;
 import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 import no.fint.model.utdanning.timeplan.Fag;
+import no.fint.model.utdanning.timeplan.Faggruppe;
 import no.fint.model.utdanning.timeplan.Faggruppemedlemskap;
 import no.fint.model.utdanning.timeplan.Rom;
 import no.fint.model.utdanning.timeplan.Time;
@@ -15,6 +16,7 @@ public class LinkMapper {
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String,String>builder()
             .put(Fag.class.getName(), contextPath + RestEndpoints.FAG)
+            .put(Faggruppe.class.getName(), contextPath + RestEndpoints.FAGGRUPPE)
             .put(Faggruppemedlemskap.class.getName(), contextPath + RestEndpoints.FAGGRUPPEMEDLEMSKAP)
             .put(Rom.class.getName(), contextPath + RestEndpoints.ROM)
             .put(Time.class.getName(), contextPath + RestEndpoints.TIME)
@@ -22,7 +24,6 @@ public class LinkMapper {
             .put(Undervisningsgruppemedlemskap.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPEMEDLEMSKAP)
             .put("no.fint.model.utdanning.utdanningsprogram.Programomrade", "/utdanning/utdanningsprogram/programomrade")
             .put("no.fint.model.utdanning.elev.Elevtilrettelegging", "/utdanning/elev/elevtilrettelegging")
-            .put("no.fint.model.utdanning.timeplan.Faggruppe", "/utdanning/timeplan/faggruppe")
             .put("no.fint.model.utdanning.utdanningsprogram.Skole", "/utdanning/utdanningsprogram/skole")
             .put("no.fint.model.utdanning.vurdering.Eksamensgruppe", "/utdanning/vurdering/eksamensgruppe")
             .put("no.fint.model.utdanning.elev.Medlemskap", "/utdanning/elev/medlemskap")
