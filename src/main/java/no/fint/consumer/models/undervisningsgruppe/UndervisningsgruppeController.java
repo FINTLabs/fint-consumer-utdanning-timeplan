e00947c4879c14c41a259c845980e623aa3051ab
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2024-06-18 11:26:30 +0200

    Version 3.18.0. (#26)

--- src/main/java/no/fint/consumer/models/undervisningsgruppe/UndervisningsgruppeController.java
+++ src/main/java/no/fint/consumer/models/undervisningsgruppe/UndervisningsgruppeController.java
@@ -182,7 +182,7 @@ public class UndervisningsgruppeController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(undervisningsgruppe);
+            return linker.mapAndResetLinks(undervisningsgruppe);
         }    
     }
 
