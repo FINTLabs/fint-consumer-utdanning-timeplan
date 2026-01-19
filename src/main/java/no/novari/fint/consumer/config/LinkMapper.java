0216440d55f0c0232a954649dc2d5c5bce53cfac
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2026-01-19 15:10:55 +0100

    Version 4.0.0-rc-6. (#37)
    * Version 4.0.0-rc-6.
    
    * refactor and add odatafilter

---
+++ src/main/java/no/novari/fint/consumer/config/LinkMapper.java
@@ -0,0 +1,42 @@
+package no.novari.fint.consumer.config;
+
+import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+import no.novari.fint.consumer.utils.RestEndpoints;
+import no.novari.fint.model.utdanning.timeplan.Eksamen;
+import no.novari.fint.model.utdanning.timeplan.Fag;
+import no.novari.fint.model.utdanning.timeplan.Faggruppe;
+import no.novari.fint.model.utdanning.timeplan.Faggruppemedlemskap;
+import no.novari.fint.model.utdanning.timeplan.Rom;
+import no.novari.fint.model.utdanning.timeplan.Time;
+import no.novari.fint.model.utdanning.timeplan.Undervisningsgruppe;
+import no.novari.fint.model.utdanning.timeplan.Undervisningsgruppemedlemskap;
+
+public class LinkMapper {
+
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String,String>builder()
+            .put(Eksamen.class.getName(), contextPath + RestEndpoints.EKSAMEN)
+            .put(Fag.class.getName(), contextPath + RestEndpoints.FAG)
+            .put(Faggruppe.class.getName(), contextPath + RestEndpoints.FAGGRUPPE)
+            .put(Faggruppemedlemskap.class.getName(), contextPath + RestEndpoints.FAGGRUPPEMEDLEMSKAP)
+            .put(Rom.class.getName(), contextPath + RestEndpoints.ROM)
+            .put(Time.class.getName(), contextPath + RestEndpoints.TIME)
+            .put(Undervisningsgruppe.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPE)
+            .put(Undervisningsgruppemedlemskap.class.getName(), contextPath + RestEndpoints.UNDERVISNINGSGRUPPEMEDLEMSKAP)
+            .put("no.novari.fint.model.utdanning.vurdering.Eksamensgruppe", "/model/utdanning/vurdering/eksamensgruppe")
+            .put("no.novari.fint.model.utdanning.utdanningsprogram.Programomrade", "/model/utdanning/utdanningsprogram/programomrade")
+            .put("no.novari.fint.model.utdanning.elev.Elevtilrettelegging", "/model/utdanning/elev/elevtilrettelegging")
+            .put("no.novari.fint.model.utdanning.utdanningsprogram.Skole", "/model/utdanning/utdanningsprogram/skole")
+            .put("no.novari.fint.model.utdanning.kodeverk.Skolear", "/model/utdanning/kodeverk/skolear")
+            .put("no.novari.fint.model.utdanning.kodeverk.Fagmerknad", "/model/utdanning/kodeverk/fagmerknad")
+            .put("no.novari.fint.model.utdanning.kodeverk.Fagstatus", "/model/utdanning/kodeverk/fagstatus")
+            .put("no.novari.fint.model.utdanning.elev.Elevforhold", "/model/utdanning/elev/elevforhold")
+            .put("no.novari.fint.model.utdanning.elev.Varsel", "/model/utdanning/elev/varsel")
+            .put("no.novari.fint.model.utdanning.elev.Undervisningsforhold", "/model/utdanning/elev/undervisningsforhold")
+            .put("no.novari.fint.model.utdanning.kodeverk.Termin", "/model/utdanning/kodeverk/termin")
+            /* .put(TODO,TODO) */
+            .build();
+    }
+
+}
---
+++ src/main/java/no/novari/fint/consumer/models/undervisningsgruppemedlemskap/UndervisningsgruppemedlemskapLinker.java
@@ -0,0 +1,64 @@
+package no.novari.fint.consumer.models.undervisningsgruppemedlemskap;
+
+import no.novari.fint.model.resource.utdanning.timeplan.UndervisningsgruppemedlemskapResource;
+import no.novari.fint.model.resource.utdanning.timeplan.UndervisningsgruppemedlemskapResources;
+import no.novari.fint.relations.FintLinker;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+public class UndervisningsgruppemedlemskapLinker extends FintLinker<UndervisningsgruppemedlemskapResource> {
+
+    public UndervisningsgruppemedlemskapLinker() {
+        super(UndervisningsgruppemedlemskapResource.class);
+    }
+
+    public void mapLinks(UndervisningsgruppemedlemskapResource resource) {
+        super.mapLinks(resource);
+    }
+
+    @Override
+    public UndervisningsgruppemedlemskapResources toResources(Collection<UndervisningsgruppemedlemskapResource> collection) {
+        return toResources(collection.stream(), 0, 0, collection.size());
+    }
+
+    @Override
+    public UndervisningsgruppemedlemskapResources toResources(Stream<UndervisningsgruppemedlemskapResource> stream, int offset, int size, int totalItems) {
+        UndervisningsgruppemedlemskapResources resources = new UndervisningsgruppemedlemskapResources();
+        stream.map(this::toResource).forEach(resources::addResource);
+        addPagination(resources, offset, size, totalItems);
+        return resources;
+    }
+
+    @Override
+    public String getSelfHref(UndervisningsgruppemedlemskapResource undervisningsgruppemedlemskap) {
+        return getAllSelfHrefs(undervisningsgruppemedlemskap).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(UndervisningsgruppemedlemskapResource undervisningsgruppemedlemskap) {
+        Stream.Builder<String> builder = Stream.builder();
+        if (!isNull(undervisningsgruppemedlemskap.getSystemId()) && !isEmpty(undervisningsgruppemedlemskap.getSystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(undervisningsgruppemedlemskap.getSystemId().getIdentifikatorverdi(), "systemid"));
+        }
+        
+        return builder.build();
+    }
+
+    int[] hashCodes(UndervisningsgruppemedlemskapResource undervisningsgruppemedlemskap) {
+        IntStream.Builder builder = IntStream.builder();
+        if (!isNull(undervisningsgruppemedlemskap.getSystemId()) && !isEmpty(undervisningsgruppemedlemskap.getSystemId().getIdentifikatorverdi())) {
+            builder.add(undervisningsgruppemedlemskap.getSystemId().getIdentifikatorverdi().hashCode());
+        }
+        
+        return builder.build().toArray();
+    }
+
+}
+
